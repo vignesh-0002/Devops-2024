3f15613d708ddf9f1be6ab5836b7420c10d25635
vignesh-0002 <65174215+vignesh-0002@users.noreply.github.com>
2025-04-17 00:38:19 +0530

    Update main.tf

--- Terraform/EKS-cluster/main.tf
+++ Terraform/EKS-cluster/main.tf
@@ -1,49 +1,20 @@
-# vpc/main.tf
-resource "aws_vpc" "main" {
-  cidr_block = var.vpc_cidr
-  enable_dns_support = true
-  enable_dns_hostnames = true
-  tags = {
-    Name = "main-vpc"
-  }
+# main.tf
+provider "aws" {
+  region = "us-west-2"
 }
 
-resource "aws_subnet" "subnet_public" {
-  count = length(var.public_subnets)
-  vpc_id = aws_vpc.main.id
-  cidr_block = element(var.public_subnets, count.index)
-  availability_zone = element(var.availability_zones, count.index)
-  map_public_ip_on_launch = true
-  tags = {
-    Name = "public-subnet-${count.index}"
-  }
+module "vpc" {
+  source        = "./vpc"
+  vpc_cidr      = "10.0.0.0/16"
+  public_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
+  private_subnets = ["10.0.3.0/24", "10.0.4.0/24"]
+  availability_zones = ["us-west-2a", "us-west-2b"]
 }
 
-resource "aws_subnet" "subnet_private" {
-  count = length(var.private_subnets)
-  vpc_id = aws_vpc.main.id
-  cidr_block = element(var.private_subnets, count.index)
-  availability_zone = element(var.availability_zones, count.index)
-  tags = {
-    Name = "private-subnet-${count.index}"
-  }
-}
-
-resource "aws_security_group" "eks_sg" {
-  vpc_id = aws_vpc.main.id
-  name   = "eks-security-group"
-  description = "Allow inbound SSH and EKS traffic"
-
-  ingress {
-    from_port   = 0
-    to_port     = 65535
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  egress {
-    from_port   = 0
-    to_port     = 65535
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+module "eks" {
+  source              = "./eks"
+  cluster_name        = "my-eks-cluster"
+  eks_role_arn        = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  subnet_ids          = module.vpc.public_subnets
+  eks_security_group  = module.vpc.eks_security_group
 }
