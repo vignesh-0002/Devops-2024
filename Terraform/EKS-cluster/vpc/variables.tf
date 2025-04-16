271c11b9635d156c24ad5c2b8ffd34ac5a3f566e
vignesh-0002 <65174215+vignesh-0002@users.noreply.github.com>
2025-04-17 00:40:10 +0530

    Create variables.tf

---
+++ Terraform/EKS-cluster/vpc/variables.tf
@@ -0,0 +1,24 @@
+# vpc/variables.tf
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "public_subnets" {
+  description = "List of public subnet CIDR blocks"
+  type        = list(string)
+  default     = ["10.0.1.0/24", "10.0.2.0/24"]
+}
+
+variable "private_subnets" {
+  description = "List of private subnet CIDR blocks"
+  type        = list(string)
+  default     = ["10.0.3.0/24", "10.0.4.0/24"]
+}
+
+variable "availability_zones" {
+  description = "List of availability zones"
+  type        = list(string)
+  default     = ["us-west-2a", "us-west-2b"]
+}
