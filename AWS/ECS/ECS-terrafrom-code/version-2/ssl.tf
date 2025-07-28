4bb02972e1f785aa6015054893ee2142d9a04e1d
vignesh-0002 <65174215+vignesh-0002@users.noreply.github.com>
2025-07-29 00:07:47 +0530

    Update ssl.tf

--- AWS/ECS/ECS-terrafrom-code/version-2/ssl.tf
+++ AWS/ECS/ECS-terrafrom-code/version-2/ssl.tf
@@ -1,113 +1,47 @@
-resource "aws_vpc" "main" {
- cidr_block           = "10.0.0.0/16"
- enable_dns_hostnames = true
- enable_dns_support   = true
- tags = {
-   name = "main"
- }
+# www.getitworth.com -> ALB
+resource "cloudflare_record" "www" {
+  zone_id = "6a467531e283a3e42ed05e8dde1e45ef"
+  name    = "www"
+  type    = "CNAME"
+  value   = aws_lb.ecs_alb.dns_name
+  proxied = true
 }
 
-resource "aws_subnet" "public_subnet_a" {
- vpc_id                  = aws_vpc.main.id
- cidr_block              = cidrsubnet(aws_vpc.main.cidr_block, 8, 1)
- map_public_ip_on_launch = true
- availability_zone       = "us-east-1a"
-
+# getitworth.com -> ALB via Cloudflare CNAME flattening
+resource "cloudflare_record" "root" {
+  zone_id = "6a467531e283a3e42ed05e8dde1e45ef"
+  name    = "getitworth.com"
+  type    = "CNAME"
+  value   = aws_lb.ecs_alb.dns_name
+  proxied = true
 }
 
-resource "aws_subnet" "public_subnet_b" {
- vpc_id                  = aws_vpc.main.id
- cidr_block              = cidrsubnet(aws_vpc.main.cidr_block, 8, 2)
- map_public_ip_on_launch = true
- availability_zone       = "us-east-1b"
+resource "aws_acm_certificate" "ssl_cert" {
+  domain_name       = "getitworth.com"
+  validation_method = "DNS"
 
-}
-
-
-#`````````````````
-# Public Subnet A
-resource "aws_subnet" "private_subnet_a" {
-  vpc_id                  = aws_vpc.main.id
-  cidr_block              = cidrsubnet(aws_vpc.main.cidr_block, 8, 3)
-  map_public_ip_on_launch = false
-  availability_zone       = "us-east-1a"
-}
-
-# Private Subnet B
-resource "aws_subnet" "private_subnet_b" {
-  vpc_id                  = aws_vpc.main.id
-  cidr_block              = cidrsubnet(aws_vpc.main.cidr_block, 8, 4)
-  map_public_ip_on_launch = false
-  availability_zone       = "us-east-1b"
-}
-
-
-
-resource "aws_eip" "nat_eip" {
-    depends_on = [aws_internet_gateway.internet_gateway]
-}
-
-resource "aws_nat_gateway" "nat" {
-  allocation_id = aws_eip.nat_eip.id
-  subnet_id     = aws_subnet.public_subnet_a.id
-}
-
-
-resource "aws_route_table" "private_route_table" {
-  vpc_id = aws_vpc.main.id
-
-  route {
-    cidr_block     = "0.0.0.0/0"
-    nat_gateway_id = aws_nat_gateway.nat.id
+  tags = {
+    Name = "getitworth-ssl"
   }
 }
 
-resource "aws_route_table_association" "private_subnet_a" {
-  subnet_id      = aws_subnet.private_subnet_a.id
-  route_table_id = aws_route_table.private_route_table.id
-}
-
-resource "aws_route_table_association" "private_subnet_b" {
-  subnet_id      = aws_subnet.private_subnet_b.id
-  route_table_id = aws_route_table.private_route_table.id
-}
 
 
-
-#````````````````
-
-resource "aws_internet_gateway" "internet_gateway" {
- vpc_id = aws_vpc.main.id
- tags = {
-   Name = "internet_gateway"
- }
-}
-
-resource "aws_route_table" "route_table" {
- vpc_id = aws_vpc.main.id
- route {
-   cidr_block = "0.0.0.0/0"
-   gateway_id = aws_internet_gateway.internet_gateway.id
- }
-}
-
-#resource "aws_route_table_association" "subnet_route" {
-# subnet_id      = aws_subnet.subnet.id
-# route_table_id = aws_route_table.route_table.id
-#}
-#
-#resource "aws_route_table_association" "subnet2_route" {
-# subnet_id      = aws_subnet.subnet2.id
-# route_table_id = aws_route_table.route_table.id
+#output "ssl_validation_record" {
+#  value = aws_acm_certificate.ssl_cert.domain_validation_options[0]
 #}
 
 
-resource "aws_route_table_association" "subnet_route" {
-  subnet_id      = aws_subnet.public_subnet_a.id
-  route_table_id = aws_route_table.route_table.id
+
+output "ssl_validation_record" {
+  value = tolist(aws_acm_certificate.ssl_cert.domain_validation_options)[0]
 }
 
-resource "aws_route_table_association" "subnet2_route" {
-  subnet_id      = aws_subnet.public_subnet_b.id
-  route_table_id = aws_route_table.route_table.id
+
+resource "cloudflare_record" "acm_validation" {
+  zone_id = "6a467531e283a3e42ed05e8dde1e45ef"
+  name    = "_ed838db0fa0fc4ffbebeb27c827c5ad6.getitworth.com"
+  type    = "CNAME"
+  value   = "_047313bca67e63a22ea629d24d9e0662.xlfgrmvvlj.acm-validations.aws"
+  proxied = false  # MUST be false for validation to work
 }
